f4d2c45deda334f275396ed1edfda7a676aa64b4
antonymss <demeshkevichanton@gmail.com>
2021-03-02 11:38:30 +0300

    refact

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -5,26 +5,26 @@ export default {
 }
 
 export const SimpleExample = () => {
-    const [fake,setFake] = useState(1)
+    const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
 
     console.log('SimpleExample')
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect every render')
         document.title = counter.toString()
     })
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect only first render (componentDidMount)')
         document.title = counter.toString()
-    },[])
-    useEffect(()=>{
+    }, [])
+    useEffect(() => {
         console.log('useEffect first render and every counter change')
         document.title = counter.toString()
-    },[counter])
+    }, [counter])
 
     return <>
-        Hello,  {counter}, {fake}
+        Hello, {counter}, {fake}
         <button onClick={() => setFake(fake + 1)}>fake+</button>
         <button onClick={() => setCounter(counter + 1)}>counter+</button>
 
@@ -32,42 +32,49 @@ export const SimpleExample = () => {
 }
 
 export const SetTimeoutExample = () => {
-    const [fake,setFake] = useState(1)
+    const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
 
     console.log('SetTimeoutExample')
 
-    useEffect(()=>{
-        setTimeout(()=> {
+    useEffect(() => {
+        setTimeout(() => {
             console.log('SetTimeout')
             document.title = counter.toString()
         }, 1000)
 
-    },[counter])
+    }, [counter])
 
     return <>
-        Hello,  {counter} {fake}
+        Hello, {counter} {fake}
         <button onClick={() => setFake(fake + 1)}>fake+</button>
         <button onClick={() => setCounter(counter + 1)}>counter+</button>
 
     </>
 }
 
-export const SetIntervalExample = () => {
+export const ResetEffectExample = () => {
     const [counter, setCounter] = useState(1)
 
-    console.log('SetIntervalExample')
+    console.log('Component rendered')
 
-    useEffect(()=>{
-        setInterval(()=> {
-            setCounter(state=> state +1)
+    useEffect(() => {
+        console.log('Effect occurred' + counter)
 
-        }, 1000)
+        return()=>{
+            console.log('Reset Effect')
+        }
 
-    },[])
+    }, [counter])
+
+    const increase = () => {
+        setCounter(counter + 1)
+    }
 
     return <>
         Hello, counter {counter}
+        <button onClick={increase}>+
+        </button>
 
 
     </>
